5b285f7ac9068ee8250f587fecf0ff3decacc74c
farhan <farhanasfar21@gmail.com>
2026-02-24 16:42:41 +0600

    Add listing display in admin panel

--- automation/admin.py
+++ automation/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from automation.models import TestResult
+from automation.models import TestResult, Listing
 
 
 @admin.register(TestResult)
@@ -8,4 +8,12 @@ class TestResultAdmin(admin.ModelAdmin):
     list_filter = ('passed',)
     search_fields = ('testCase', 'comment')
     readonly_fields = ('testCase', 'url', 'passed', 'comment', 'created_at')
+    ordering = ('-created_at',)
+
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'search_url', 'img_url','location','checkin', 'checkout', 'guests','created_at')
+    list_filter = ('location',)
+    search_fields = ('title', 'location')
+    readonly_fields = ('price', 'search_url', 'img_url', 'title', 'created_at')
     ordering = ('-created_at',)
